d4db41dd86bc7c6324086b89f4c017ee406b68de
Jonathan <potatostew99@gmail.com>
2023-05-15 11:06:22 -0400

    hey

--- Main.java
+++ Main.java
@@ -1,3 +1,3 @@
 public class Main{
-  System.out.println("Most people can't comprehend the beauty that the eye is the BEE- holder")
+  System.out.println("MMost people can't comprehend the beauty that the eye is the BEE- holder")
 }
