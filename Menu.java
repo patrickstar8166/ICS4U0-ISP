94723ae84ff454e1777754e439adfb513ef1c61e
patrickstar8166 <patrick201909@gmail.com>
2023-05-17 11:39:56 -0400

    lesgo it finally work :D

--- Menu.java
+++ Menu.java
@@ -1,52 +1,49 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class Menu implements ActionListener{
+public class Menu extends JFrame implements ActionListener{
    JButton start, instr;
-   JFrame frame;
-   JPanel panel1, panel2;
-   Drawing draw;
+   JLayeredPane pane;
+   Drawing draw; 
    
    public Menu(){ 
       start = new JButton("Start");
       instr = new JButton("Instructions");
-      frame = new JFrame("Menu");
-      panel1 = new JPanel();
-      panel2 = new JPanel();
+      pane = getLayeredPane();
       draw = new Drawing();
    }
 
    public void display(){
-      frame.setResizable(false);
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(1000, 680);
+      //frame
+      this.setResizable(false);
+      this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      this.setSize(1000, 680);
       
-      panel1.setLayout(new BoxLayout(panel1, BoxLayout.Y_AXIS));
-      panel1.setPreferredSize(1000, 400);
-      start.setAlignmentX(Component.CENTER_ALIGNMENT);
-      start.setPreferredSize(new Dimension(100, 40));
-      panel1.add(start);
-      panel1.add(Box.createRigidArea(new Dimension(0, 20)));
-      instr.setAlignmentX(Component.CENTER_ALIGNMENT);
-      instr.setPreferredSize(new Dimension(100, 1000));
-      panel1.add(instr);  
+      start.setBackground(Color.LIGHT_GRAY);
+      start.setBounds(450, 300, 100, 40);
+      pane.add(start, 1);
       
-      panel2.setLayout(new GridLayout(2, 1));
-      panel2.add(draw);
-      panel2.add(panel1);
+      instr.setBackground(Color.LIGHT_GRAY);
+      instr.setBounds(425, 375, 150, 40);
+      pane.add(instr, 1);
       
-      frame.add(panel2, BorderLayout.CENTER);
-      frame.setVisible(true);
+      this.add(draw);
+      this.setVisible(true);
    }
       
    class Drawing extends JComponent{
       public void paint(Graphics g){
+         //background
+         g.setColor(new Color(173, 216, 230));
+         g.fillRect(0, 0, 1000, 680);
+         
+         //text
          g.setColor(Color.black);
          g.setFont(new Font("Sans Serif", Font.BOLD, 40));
          g.drawString("Fresh Forage Adventure", 275, 200);
       }
    }
-   
+      
    public void actionPerformed (ActionEvent e){
    }
 }
