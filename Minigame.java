1aba2b2f506a6e8e1a7f802714280a16bc0db627
remylia <darwin.decaire@gmail.com>
2023-05-17 11:40:53 -0400

    doesnt work yet

--- Minigame.java
+++ Minigame.java
@@ -5,18 +5,23 @@ import javax.swing.*;
 public class Minigame implements KeyListener{
    
    Drawing d = new Drawing();
-   private int x,y;
+   private boolean rightHeld, leftHeld,end;
+   Player p = new Player();
+   private double timer;
    
+   ArrayList<FallingObject> f = new ArrayList<FallingObject>();
    public Minigame(){
-      x = 490;
-      y = 500;
+      timer = 0;
+      rightHeld = false; 
+      leftHeld = false;
+      end = false;
       JFrame frame = new JFrame("Minigame");
       frame.setSize(1000,680);
       frame.add(d);
       frame.setVisible(true);
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      d.addKeyListener(this);
-
+      frame.addKeyListener(this);
+      
    }
    
    class Drawing extends JComponent
@@ -24,34 +29,124 @@ public class Minigame implements KeyListener{
       public void paint (Graphics g)
       {
          g.setColor(Color.blue);
-         g.fillOval(x,y,20,20);
+         g.fillRect(p.getX(),p.getY(),50,100);
+         g.setColor(Color.red);
+         g.fillRect(p.getX()-50,p.getY()-30,150,30);
+         for(int i = 0; i<f.size(); i++){
+            g.fillRect(f.get(i).getX(),f.get(i).getY(),20,20);
+         }
+         
       }
+   }
+   public void display(){ //REALLY needs implementation of runnable, this is super jank
+      while(!end){
+         //System.out.println("X: "+p.getX()+ " Y: "+p.getY());
+         timer+= 0.0001;
+         if(rightHeld^leftHeld){
+            p.move(rightHeld,leftHeld);
+         }
+         if(timer>1){
+            timer = 0;
+            f.add(generate());
+         }
+         
+         d.repaint();
+         
+         
+      }
+   }
+   
+   public FallingObject generate(){
+      return new FallingObject(Math.random()*1000);
    }
    
    public void keyPressed(KeyEvent e){
       if(e.getKeyCode()==e.VK_RIGHT){
-         x++;
-         System.out.println("moving right: "+x);
-         d.repaint();
+         rightHeld = true;
+         
       }
       if(e.getKeyCode() == e.VK_LEFT){
-         x--;
-         System.out.println("moving left: "+x);
-         d.repaint();
+         leftHeld = true;
+         
       }
    }
    
    public void keyReleased(KeyEvent e){
-   
+      if(e.getKeyCode()==e.VK_RIGHT){
+         rightHeld = false;
+         System.out.println("stopping movement");
+      }
+      if(e.getKeyCode() == e.VK_LEFT){
+         leftHeld = false;
+         System.out.println("stopping movement");
+      }
    }
    
    public void keyTyped(KeyEvent e){
    
    }
+   class Player{
+      private double x,y;
+      public Player(){
+         x = 490;
+         y = 420;
+      }
+      public Player(double x, double y){
+         this.x = x;
+         this.y = y;
+      }
+      
+      public void move(boolean right, boolean left){
+         if(left){
+            x-=0.00008;
+         }
+         else if(right){
+            x+=0.00008;
+         }
+         if(x<50){
+            x = 50;
+         }
+         else if(x>900){
+            x = 900;
+         }
+      }
+      
+      public int getX(){
+         return (int)x;
+      }
+      
+      public int getY(){
+         return (int)y;
+      }
+   }
+   
+   class FallingObject{
+      private int x;
+      private double y;
+      public FallingObject(int x){
+         this.x = x;
+         y = -20;
+      }
+      
+      public int getX(){
+         return x;
+      }
+      
+      public int getY(){
+         return (int) y;
+      }
+      public void fall(){
+         y+=0.0001;
+      }
+      
+   
+   }
    
    public static void main(String[] args)
    {
-      new Minigame();
+      Minigame m = new Minigame();
+      m.display();
+      //m.display(player);
    }
 
 }
